043c11aec07c89cf6302d1a721bcfe1433b9c59e
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-10-26 15:29:39 -0800

    check for success substring in px4_set_time.sh

--- scripts/px4_set_time.sh
+++ scripts/px4_set_time.sh
@@ -5,11 +5,11 @@ HOST_PORT=14569
 UNIX_EPOCH_TIME=$(date +%s) # %s = seconds since the Epoch (1970-01-01 00:00 UTC)
 output=$(python3 /usr/bin/px4_shell_command.py -p "udp:$HOST_IP:$HOST_PORT" "system_time set $UNIX_EPOCH_TIME")
 
-if [ -z $output ]; then
-	echo "Setting time failed!"
-	echo "output: $output"
+if [[ $output != *"Successfully set system time"* ]]; then
+	echo "Failed to set system time!"
+	echo "response: $output"
 	exit 1
 fi
 
-echo "Setting time succeed!"
+echo "Successfully set system time"
 echo "output: $output"
