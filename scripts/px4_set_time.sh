9cca4c77855dac107f8c0afcbeeb05caa9312a3b
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-10-26 15:02:21 -0800

    exit error if settime fails

--- scripts/px4_set_time.sh
+++ scripts/px4_set_time.sh
@@ -3,4 +3,13 @@ HOST_IP=127.0.0.1
 HOST_PORT=14551
 
 UNIX_EPOCH_TIME=$(date +%s) # %s = seconds since the Epoch (1970-01-01 00:00 UTC)
-python3 /usr/bin/px4_shell_command.py -p "udp:$HOST_IP:$HOST_PORT" "system_time set $UNIX_EPOCH_TIME"
+output=$(python3 /usr/bin/px4_shell_command.py -p "udp:$HOST_IP:$HOST_PORT" "system_time set $UNIX_EPOCH_TIME")
+
+if [ -z $output ]; then
+	echo "Setting time failed!"
+	echo "output: $output"
+	exit 1
+fi
+
+echo "Setting time succeed!"
+echo "output: $output"
