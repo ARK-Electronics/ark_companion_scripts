8c3c966a01cb6314be33256d5f9898d08bd1228b
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-06-27 11:06:04 -0800

    hotspot_fallback.sh: wait for wireless interface to come up before performing logic for station/hotspot

--- platform/common/wifi/hotspot_fallback.sh
+++ platform/common/wifi/hotspot_fallback.sh
@@ -1,43 +1,53 @@
 #!/bin/bash
 
 find_existing_ap() {
-    local IFS=$'\n'  # Change the Internal Field Separator to new line, so 'read' reads one line at a time
-    local connections=$(nmcli -t -f NAME,TYPE con show | grep "802-11-wireless")
+	local IFS=$'\n'  # Change the Internal Field Separator to new line, so 'read' reads one line at a time
+	local connections=$(nmcli -t -f NAME,TYPE con show | grep "802-11-wireless")
 
-    echo "$connections" | while read -r connection; do
-        local name=$(echo "$connection" | cut -d ':' -f1)
-        local mode=$(nmcli -t -f 802-11-wireless.mode con show "$name")
-        if [[ $mode == "802-11-wireless.mode:ap" ]]; then
-            echo "$name"
-            return
-        fi
-    done
-    echo ""
+	echo "$connections" | while read -r connection; do
+		local name=$(echo "$connection" | cut -d ':' -f1)
+		local mode=$(nmcli -t -f 802-11-wireless.mode con show "$name")
+		if [[ $mode == "802-11-wireless.mode:ap" ]]; then
+			echo "$name"
+			return
+		fi
+	done
+	echo ""
 }
 
-# sleep for 30 seconds
-# If NM isn't connected start the hotspot
-sleep 30
+# Endlessly loop until a wireless interface becomes available
+while true; do
+	INTERFACE=$(iw dev | grep Interface | awk '{print $2}')
+	if [ ! -z "$INTERFACE" ]; then
+		echo "Found wireless interface: $INTERFACE"
+		# Give it a second to come up
+		sleep 5
+		break  # Exit the loop when the interface is found
+	else
+		echo "No wireless interface found, retrying in 5 seconds..."
+		sleep 5
+	fi
+done
 
 wifi_connected=$(nmcli con show --active | grep wifi)
 if [ -n "$wifi_connected" ]; then
-	echo "WiFi is connected, not setting up hotspot"
+	ssid=$(echo "$wifi_connected" | awk '{print $1}')
+	echo "WiFi is connected to $ssid"
 	exit 0
 fi
 
+echo "WiFi is not connected, setting up hotspot"
 AP_SSID="$(find_existing_ap)"
 
-if [ -n "$AP_SSID" ]; then
-	# Access point exists, start it
-	nmcli con up "$AP_SSID"
- else
- 	echo "Creating new hotspot"
+# Check if the access point SSID is empty and needs creating
+if [ -z "$AP_SSID" ]; then
+	echo "Creating new hotspot"
 	# Create default hotspot
 	AP_SSID="ARK_Hotspot"
 	AP_PASSWORD="password"
 	nmcli con add type wifi ifname '*' con-name "$AP_SSID" autoconnect no ssid "$AP_SSID" 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
 	nmcli con modify "$AP_SSID" wifi-sec.key-mgmt wpa-psk wifi-sec.psk "$AP_PASSWORD" 802-11-wireless-security.pmf disable
-	nmcli con up "$AP_SSID"
 fi
 
 echo "Starting hotspot: $AP_SSID"
+nmcli con up "$AP_SSID"
