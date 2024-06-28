651611c645b0cafc9050cf1030bd482c70067bca
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-06-28 15:40:55 -0800

    update get_service_status.sh

--- platform/common/scripts/get_service_status.sh
+++ platform/common/scripts/get_service_status.sh
@@ -9,17 +9,14 @@ fi
 SERVICE_NAME=$1
 
 # Get the status of the service
-SERVICE_STATUS=$(systemctl --user status $SERVICE_NAME 2>/dev/null)
+ENABLED_STATUS=$(systemctl --user is-enabled $SERVICE_NAME 2>&1)
+ACTIVE_STATUS=$(systemctl --user is-active $SERVICE_NAME 2>&1)
 
-# Check if systemctl command failed (service does not exist)
-if [ $? -ne 0 ]; then
+# Check for 'Failed to get unit file state' indicating the service does not exist
+if [[ "$ENABLED_STATUS" =~ "Failed to get unit file state" ]]; then
     echo "{\"status\": \"error\", \"message\": \"Service not found\"}"
     exit 1
 fi
 
-# Extracting 'Loaded' and 'Active' lines and parse needed details
-LOADED_STATUS=$(echo "$SERVICE_STATUS" | grep "Loaded" | awk -F';' '{print $2}' | xargs)
-ACTIVE_STATUS=$(echo "$SERVICE_STATUS" | grep "Active" | awk '{print $2}')
-
 # Generate JSON output
-echo "{\"status\": \"success\", \"service\": \"${SERVICE_NAME}\", \"loaded\": \"${LOADED_STATUS}\", \"active\": \"${ACTIVE_STATUS}\"}"
+echo "{\"status\": \"success\", \"service\": \"${SERVICE_NAME}\", \"enabled\": \"${ENABLED_STATUS}\", \"active\": \"${ACTIVE_STATUS}\"}"
