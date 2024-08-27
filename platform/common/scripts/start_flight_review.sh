3b990f88720f1c3651253ad70ad93debafdb34b0
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-08-26 23:07:29 -0800

    added flight review startup script, use hostname in bash for server

---
+++ platform/common/scripts/start_flight_review.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+HOSTNAME=$(hostname -f)
+PORT=5006
+
+python3 /opt/flight_review/app/serve.py --port=$PORT --address=0.0.0.0 --use-xheaders --allow-websocket-origin=$HOSTNAME--allow-websocket-origin=$HOSTNAME:$PORT
