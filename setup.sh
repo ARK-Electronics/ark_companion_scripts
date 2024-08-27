5b407c38e291bf8ced0db5e842a76f6c012ac390
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-08-27 11:03:01 -0800

    silence pushd

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OUTPUT_FILE="output.txt"
-pushd .
+pushd . &>/dev/null
 cd "$(dirname "$0")"
 ./setup/install_software.sh 2>&1 | while IFS= read -r line; do
     echo "$(date +"[%H:%M:%S]") $line"
