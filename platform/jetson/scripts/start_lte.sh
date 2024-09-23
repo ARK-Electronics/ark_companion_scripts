1ba207c4c7e2d6f6d81cb698d7901c3ae812031c
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-09-22 17:36:42 -0800

    spin until modem detected

--- platform/jetson/scripts/start_lte.sh
+++ platform/jetson/scripts/start_lte.sh
@@ -2,18 +2,31 @@
 
 APN="fast.t-mobile.com"
 
-sudo mmcli -m 0 --3gpp-set-initial-eps-bearer-settings="apn=$APN"
-sudo mmcli -m 0 --simple-connect="apn=$APN,ip-type=ipv4v6"
+while true; do
+		MODEM_LIST=$(mmcli -L | grep -Eo '/Modem/[0-9]+')
+		if [ -n "$MODEM_LIST" ]; then
+				# Extract the first modem instance (if multiple modems are found)
+				MODEM_INSTANCE=$(echo "$MODEM_LIST" | head -n 1 | grep -Eo '[0-9]+')
+				echo "Modem detected: instance $MODEM_INSTANCE"
+				break
+		else
+				echo "No modem detected. Retrying in 5 seconds..."
+				sleep 5
+		fi
+done
 
-MODEM_STATE=$(mmcli -m 0 --output-keyvalue | grep -E '^modem.generic.state[[:space:]]*:' | awk -F': ' '{print $2}')
+sudo mmcli -m $MODEM_INSTANCE --3gpp-set-initial-eps-bearer-settings="apn=$APN"
+sudo mmcli -m $MODEM_INSTANCE --simple-connect="apn=$APN,ip-type=ipv4v6"
+
+MODEM_STATE=$(mmcli -m $MODEM_INSTANCE --output-keyvalue | grep -E '^modem.generic.state[[:space:]]*:' | awk -F': ' '{print $2}')
 if [ "$MODEM_STATE" != "connected" ]; then
-  echo "Modem is not connected, exiting."
-  exit 1
+	echo "Modem is not connected, exiting."
+	exit 1
 else
-  echo "Modem is connected."
+	echo "Modem is connected."
 fi
 
-BEARER_INFO=$(mmcli -m 0 --bearer=1)
+BEARER_INFO=$(mmcli -m $MODEM_INSTANCE --bearer=1)
 
 IP_ADDRESS=$(echo "$BEARER_INFO" | grep -Po '(?<=address: )[0-9.]+')
 PREFIX=$(echo "$BEARER_INFO" | grep -Po '(?<=prefix: )[0-9]+')
